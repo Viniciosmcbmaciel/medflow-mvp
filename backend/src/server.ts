6d4f1f9e314eea6167626c28ad7cfdf191957793
Viniciosmcbmaciel <vinicios.mcastelobranco@gmail.com>
2026-05-11 20:33:22 -0300

    fix railway startup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import app from "./app.js";
-import { env } from "./config/env.js";
 
-app.listen(env.port, "0.0.0.0", () => {
-  console.log(`API rodando em http://localhost:${env.port}`);
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(
+    `🚀 Server running on port ${PORT}`
+  );
 });
-
